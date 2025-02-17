8b1232a5bd2552392235cd90ddaee7c76e7b2371
Jorge Serrano <entrevistasmias@gmail.com>
2025-02-17 10:43:26 -0300

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
